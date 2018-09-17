be9b2959b10b5310afa3522b31034bcac45e2b3f
Konstantin Chupin <kchupin41@gmail.com>
2018-09-18 00:51:10 +0700

    - Bot command API normalization - New bot command API in realization + date selector proto

--- tg_notificator/tg_bot.py
+++ tg_notificator/tg_bot.py
@@ -5,33 +5,73 @@ import json
 
 from aiotg import Chat
 
-from tg_notificator.tg_bot_base import BotCommand, TgBotBase
+from tg_notificator.tg_bot_base import BotCommand, TgBotBase, InlineKeyboardMarkupData, InlineKeyboardButtonData
 
 log = logging.getLogger(__name__)
 
 
 class EchoCommand(BotCommand):
     async def run(self, initial_message: Chat):
-        await initial_message.send_text(
+        await self.send_message(
             f"Your message in API format\n"
+            f"```\n"
             f"{json.dumps(initial_message.message, indent=2)}"
+            f"```"
         )
 
 
 class RemindCommand(BotCommand):
+    async def request_date(self):
+        day = 1
+        result = await self.send_message("Select date")
+        msg_id = result["result"]["message_id"]
+
+        while True:
+            result = await self.edit_message_reply_markup(msg_id, markup=InlineKeyboardMarkupData(inline_keyboard=[
+                [
+                    InlineKeyboardButtonData(text="-", callback_data="day_dec"),
+                    InlineKeyboardButtonData(text=f"{day}", callback_data="day_click"),
+                    InlineKeyboardButtonData(text="+", callback_data="day_inc"),
+                ]
+            ]))
+            log.info(result)
+
+            cq = await self.next_callback()
+            log.info(f"CQ {cq}")
+
+            if cq.data == "day_inc":
+                day += 1
+            elif cq.data == "day_dec":
+                day -= 1
+            else:
+                break
+
+            await self.answer_callback_query(cq)
+
+        await self.answer_callback_query(cq)
+        await self.edit_message_reply_markup(msg_id, InlineKeyboardMarkupData(inline_keyboard=[[]]))
+        return day
+
     async def run(self, initial_message: Chat):
         initial_text = initial_message.message["text"]  # type: str
 
         if len(initial_text.split()) > 1:
             reminder_text = " ".join(initial_text.split()[1:])
         else:
-            await initial_message.send_text("Что напомнить?")
+            await self.send_message("Что напомнить?")
 
             remind_text_message = await self.next_message()
 
             reminder_text = remind_text_message.message["text"]
 
-        await initial_message.send_text(f"Ок. Напомню: {reminder_text}")
+        await self.send_message(text=f"Ок. Напомню: {reminder_text}")
+
+        date = await self.request_date()
+
+        await self.send_message(f"Напомню в {date}")
+        # while True:
+        #     msg = await self.next_message()
+        #     print(msg.message)
 
 
 class TgBot(TgBotBase):
@@ -39,6 +79,6 @@ class TgBot(TgBotBase):
         msg = chat_obj.message["text"]  # type: str
 
         if msg.lower().startswith("echo"):
-            return EchoCommand(self.app_wrapper.loop)
+            return EchoCommand(self.app_wrapper.loop, chat_obj)
         elif msg.lower().startswith("remind"):
-            return RemindCommand(self.app_wrapper.loop)
+            return RemindCommand(self.app_wrapper.loop, chat_obj)
--- tg_notificator/tg_bot_base.py
+++ tg_notificator/tg_bot_base.py
@@ -1,9 +1,12 @@
 from abc import ABC, abstractmethod, ABCMeta
-from typing import TYPE_CHECKING, Dict, Optional
+from typing import TYPE_CHECKING, Dict, Optional, Union, List, Set
 
 import logging
 
 from aiotg import Bot, Chat, asyncio
+import aiotg
+
+import pydantic
 
 from tg_notificator.user_registry import TgUser
 
@@ -13,29 +16,117 @@ if TYPE_CHECKING:
 log = logging.getLogger(__name__)
 
 
-# TODO CONSIDER: save initial message in constructor
+class TgModel(pydantic.BaseModel):
+    class Config:
+        TG_TYPE = None
+
+    def dict(self, *, include: Set[str] = None, exclude: Set[str] = set(), by_alias: bool = False):
+        ret = super().dict(include=include, exclude=exclude, by_alias=by_alias)
+        ret.update({"type": self.Config.TG_TYPE})
+
+        return ret
+
+
+class InlineKeyboardButtonData(TgModel):
+    class Config:
+        TG_TYPE = "InlineKeyboardButton"
+
+    text: str
+    callback_data: Optional[str] = None
+
+
+class InlineKeyboardMarkupData(TgModel):
+    class Config:
+        TG_TYPE = "InlineKeyboardMarkup"
+
+    inline_keyboard: List[List[InlineKeyboardButtonData]]
+
+
+class MessageData(TgModel):
+    class Config:
+        TG_TYPE = "Message"
+
+    message_id: int
+    text: Optional[str] = None
+
+
+class CallbackQueryData(pydantic.BaseModel):
+    class Config:
+        TG_TYPE = "CallbackQuery"
+
+    id: str
+    message: MessageData
+    data: Optional[str]
+
+
 class BotCommand(ABC):
 
-    def __init__(self, loop: asyncio.AbstractEventLoop):
+    def __init__(self, loop: asyncio.AbstractEventLoop, initial_chat_obj: Chat):
         self._q = asyncio.Queue(loop=loop)
+
+        self.bot = initial_chat_obj.bot  # type: Bot
+        self.chat_id = initial_chat_obj.id
+
+    async def edit_message_reply_markup(self, message: Union[MessageData, int], markup: InlineKeyboardMarkupData):
+        message_id = message if isinstance(message, int) else message.message_id
+
+        args = {
+            "message_id": message_id,
+            "chat_id": self.chat_id,
+        }
+
+        if markup:
+            args["reply_markup"] = markup.json()
+
+        return await self.bot.api_call("editMessageReplyMarkup", **args)
+
+    async def send_message(self, text, parse_mode="Markdown", reply_markup: InlineKeyboardMarkupData = None):
+        args = {
+            "text": text,
+            "chat_id": self.chat_id,
+            "parse_mode": parse_mode,
+        }
+
+        if reply_markup:
+            args["reply_markup"] = reply_markup.json()
+
+        return await self.bot.api_call("sendMessage", **args)
+
+    async def answer_callback_query(self, callback_query: Union[CallbackQueryData, str]):
+        callback_query_id = callback_query if isinstance(callback_query, str) else callback_query.id
+
+        return await self.bot.api_call("answerCallbackQuery", callback_query_id=callback_query_id)
 
     @abstractmethod
     async def run(self, initial_message: Chat):
         pass
 
-    async def next_message(self) -> Chat:
+    async def next_update(self) -> Union[MessageData, CallbackQueryData]:
         log.info(f"Awaiting next message")
         msg = await self._q.get()
         log.info(f"Message received {msg}")
         return msg
 
+    async def next_message(self) -> MessageData:
+        while True:
+            next_data = await self.next_update()
+            if isinstance(next_data, MessageData):
+                return next_data
 
-# TODO FIX: implement own Telegram API wrapper
+    async def next_callback(self) -> CallbackQueryData:
+        while True:
+            next_data = await self.next_update()
+            if isinstance(next_data, CallbackQueryData):
+                return next_data
+
+
 class TgBotBase(Bot, metaclass=ABCMeta):
     def __init__(self, api_token, app_wrapper: "AppWrapper", *args, **kwargs):
         super().__init__(api_token=api_token, *args, **kwargs)
         self.app_wrapper = app_wrapper
+
         self.add_command(r".*", self.handle_inbound_message)
+        self.add_callback(r".*", self.handle_inbound_message)
 
         self.map_chat_id_running_command = {}  # type: Dict[str, BotCommand]
 
@@ -89,8 +180,13 @@ class TgBotBase(Bot, metaclass=ABCMeta):
             running_command = self.map_chat_id_running_command.get(chat_obj.id)
 
             if running_command:
+                if len(args) > 0 and isinstance(args[0], aiotg.CallbackQuery):
+                    data = CallbackQueryData(**args[0].src)
+                else:
+                    data = MessageData(**chat_obj.message)
+
                 # noinspection PyProtectedMember
-                running_command._q.put_nowait(chat_obj)
+                running_command._q.put_nowait(data)
 
             else:
                 new_command = self._dispatch_initial_message(chat_obj)
